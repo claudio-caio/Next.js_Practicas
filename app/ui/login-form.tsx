adce6bdaae3a22e2d809ba5280d3f762eea18fc7
Claudio Tomadin <caioalegres@gmail.com>
2025-11-27 19:37:26 -0300

    login

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -22,7 +22,7 @@ export default function LoginForm() {
   
   
   return (
-    <form className="space-y-3">
+    <form action={formAction} method="post" className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
